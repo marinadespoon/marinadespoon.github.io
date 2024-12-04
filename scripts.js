123eba050e211810e61189cc6b28b0fa1e4aa086
marinadespoon <marinadespoon@gmail.com>
2024-12-04 23:20:29 +0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// scripts.js
-document.addEventListener('DOMContentLoaded', function() {
-    const resumeLink = document.querySelector('#resume-download a');
-    
-    resumeLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        alert('Ваше резюме успешно загружено!');
-    });
-});
